27713a809296434f24b60888bf47aa47176e5c81
therealggalba <gonmunrue@gmail.com>
2026-07-18 12:29:31 +0200

    chore: push changes inside worldcup2026

--- src/infrastructure/supabaseClient.ts
+++ src/infrastructure/supabaseClient.ts
@@ -1,10 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_SPORTS_URL || import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_SPORTS_ANON_KEY || import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("Las variables de entorno VITE_SUPABASE_URL o VITE_SUPABASE_ANON_KEY no están configuradas.");
+  throw new Error("Las variables de entorno VITE_SUPABASE_URL/VITE_SUPABASE_SPORTS_URL o sus correspondientes ANON_KEY no están configuradas.");
 }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
